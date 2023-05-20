57eab8f1da07ca87fdffc6de05e1cb146531c060
Naveen AutomationLabs <naveenanimation20@gmail.com>
2023-05-20 16:38:02 +0530

    added code for github actions

---
+++ src/test/java/mytests/GoogleTest.java
@@ -0,0 +1,24 @@
+package mytests;
+
+import base.BaseTest;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GoogleTest extends BaseTest {
+
+    @Test(priority = 1)
+    public void titleTest() {
+        String title = driver.getTitle();
+        System.out.println("page title:" + title);
+        Assert.assertEquals(title, "Google");
+    }
+
+    @Test(priority = 2)
+    public void isSearchExist() {
+        testCaseId = "4";
+        boolean flag = driver.findElement(By.name("q")).isDisplayed();
+        Assert.assertTrue(flag);
+    }
+
+}
